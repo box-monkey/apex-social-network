db3e71334fd2fef460e201ca278e4fdbf97d045d
box-monkey <jr.gasparin15@gmail.com>
2022-02-26 04:37:04 -0500

    User model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,45 @@
+const { Schema, model } = require("mongoose");
 // User
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match:
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-//     username
-//         String
-//         Unique
-//         Required
-//         Trimmed
+const User = model("User", UserSchema);
 
-//     email
-//         String
-//         Required
-//         Unique
-//         Must match a valid email address (look into Mongoose's matching validation)
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
 
-//     thoughts
-//         Array of _id values referencing the Thought model
-
-//     friends
-//         Array of _id values referencing the User model (self-reference)
-
-// Schema Settings
-
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+module.exports = User;
