bd6864be3a62f8c3999e0c7563f55cfad1239fa0
box-monkey <jr.gasparin15@gmail.com>
2022-02-26 06:27:51 -0500

    working throught thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,39 @@
 // /api/thoughts
+const router = require("express").Router();
+const {
+  getThoughts,
+  getThoughtsById,
+  updateThoughts,
+  deleteThoughts,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// api/thoughts
+router.route("/").get(getThoughts);
+
+// api/thoughts/:id
+router
+  .route("/")
+  .get(getThoughtsById)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
+
+router.route("");
+
+router.route("/:thoughtId/reactions").delete(deleteThoughts);
+
+// api/thoughts/thoughtsId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughtsId/<reactions>/<reactionId>
+router.route("toughtsId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
 
 //     GET to get all thoughts
-
 //     GET to get a single thought by its _id
-
 //     POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
 // // example data
 // {
 //   "thoughtText": "Here's a cool thought...",
@@ -14,11 +42,7 @@
 // }
 
 //     PUT to update a thought by its _id
-
 //     DELETE to remove a thought by its _id
-
 // /api/thoughts/:thoughtId/reactions
 
 //     POST to create a reaction stored in a single thought's reactions array field
-
-//     DELETE to pull and remove a reaction by the reaction's reactionId value
